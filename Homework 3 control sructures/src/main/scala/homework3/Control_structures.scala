1c38543855b4e1dee8b0fae36054799f0198681e
rakhubov <rakhubov@corp.mgaon.by>
2021-02-09 11:35:08 +0300

    first commit

--- Homework 3 control sructures/src/main/scala/homework3/Control_structures.scala
+++ Homework 3 control sructures/src/main/scala/homework3/Control_structures.scala
@@ -18,7 +18,7 @@ object Control_structures {
   }
 
   sealed trait Result
-  final case class ChangeMe(value: String) extends Result
+  final case class ChangeMe(command: Command, result: Double) extends Result {}
 
   def stringTOdouble(list: List[String]): Try[List[Double]] = {
     Try(list.map(x => x.toDouble))
@@ -27,7 +27,7 @@ object Control_structures {
   import Command._
   def parseCommand(x: String): Either[ErrorMessage, Command] = {
     x.split("\\s+").toList match {
-      case "divide" :: figures =>
+      case "divide" :: figures if figures.length == 2 =>
         stringTOdouble(figures) match {
           case Success(value) if value(1) != 0 =>
             Right(Divide(value.head, value(1)))
@@ -53,37 +53,38 @@ object Control_structures {
           case Success(value) => Right(Max(value))
           case Failure(error) => Left(ErrorMessage("max" + error.toString))
         }
-      case _ => Left(ErrorMessage("input command"))
+      case _ => Left(ErrorMessage("input command " + x))
     }
   }
 
-  def calculate(x: Command): Double =
-    x match {
-      case Divide(dividend, divisor) => dividend / divisor
-      case Sum(numbers)              => numbers.sum
-      case Average(numbers)          => numbers.sum / numbers.length
-      case Max(numbers)              => numbers.max
-      case Min(numbers)              => numbers.min
+  def calculate(command: Command): Either[ErrorMessage, Result] =
+    command match {
+      case Divide(dividend, divisor) => Right(ChangeMe(Divide(dividend, divisor), (dividend / divisor)))
+      case Sum(numbers) => Right(ChangeMe(Sum(numbers), (numbers.sum)))
+      case Average(numbers) => Right(ChangeMe(Average(numbers), (numbers.sum / numbers.length)))
+      case Max(numbers) => Right(ChangeMe(Max(numbers), numbers.max))
+      case Min(numbers) => Right(ChangeMe(Min(numbers), numbers.min))
+      case _            => Left(ErrorMessage(" dont can cancul "))
     }
 
-  def renderResult(x: Command): String =
-    x match {
-      case Divide(dividend, divisor) =>
-        s"$dividend divided by $divisor is ${calculate(x)}"
-      case Sum(figures) =>
-        s"the sum of ${figures.mkString(" ")} is ${calculate(x)}"
-      case Average(figures) =>
-        s"the average of ${figures.mkString(" ")} is ${calculate(x)}"
-      case Min(figures) =>
-        s"the minimum of ${figures.mkString(" ")} is ${calculate(x)}"
-      case Max(figures) =>
-        s"the maximum of ${figures.mkString(" ")} is ${calculate(x)}"
+  def renderResult(res: Result): String =
+    res match {
+      case ChangeMe(Divide(dividend, divisor), result) => s"$dividend divided by $divisor is $result"
+      case ChangeMe(Sum(figures), result) => s"the sum of ${figures.mkString(" ")} is $result"
+      case ChangeMe(Average(figures), result) => s"the average of ${figures.mkString(" ")} is $result"
+      case ChangeMe(Min(figures), result) => s"the minimum of ${figures.mkString(" ")} is $result"
+      case ChangeMe(Max(figures), result) => s"the maximum of ${figures.mkString(" ")} is $result"
     }
 
   def process(x: String): String = {
-    parseCommand(x) match {
-      case Right(value) => renderResult(value)
-      case Left(error)  => error.errorM
+    val process = for {
+      parce <- parseCommand(x)
+      calc <- calculate(parce)
+    } yield renderResult(calc)
+
+    process match {
+      case Right(value) => value
+      case Left(error)  => error.value
     }
   }
 
