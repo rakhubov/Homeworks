22e3e09ad72cc9e370973b3c7e1e93f7545fd03c
rakhubov <75500362+rakhubov@users.noreply.github.com>
2021-02-10 17:55:31 -0300

    Add files via upload

--- Homework 4 Collections/src/Homework4/Home4.scala
+++ Homework 4 Collections/src/Homework4/Home4.scala
@@ -1,15 +1,37 @@
 package Homework4
 
-object Solution {
+object Runsum {
   def runningSum(nums: Array[Int]): Array[Int] = {
     nums.scanLeft(0)(_ + _).tail
   }
 }
 
-object shuffl {
+object Shuffl {
   def shuffle(nums: Array[Int], n: Int): Array[Int] = {
     val start = nums.take(n)
     val end = nums.drop(n)
     (start zip end).flatMap { case (a, b) => Array(a, b) }
+  }
+}
+
+object Maximum {
+  def maximumWealth(accounts: Array[Array[Int]]): Int = {
+    accounts.flatMap { case a => Array(a.sum) }.max
+  }
+}
+
+object KidCandies {
+  def kidsWithCandies(
+      candies: Array[Int],
+      extraCandies: Int
+  ): Array[Boolean] = {
+    candies.map(_ + extraCandies >= candies.max)
+  }
+}
+
+object Maxwidth {
+  def maxWidthOfVerticalArea(points: Array[Array[Int]]): Int = {
+    val xs = points.map(_.head).sorted
+    xs.zip(xs.tail).flatMap { case (a, b) => Array(b - a) }.max
   }
 }
--- Homework 4 Collections/src/Homework4/hw4.sc
+++ Homework 4 Collections/src/Homework4/hw4.sc
@@ -1,14 +1,18 @@
 
 
-  def maximumWealth(accounts: Array[Array[Int]]): Int = {
-    5
-  }
+def maxWidthOfVerticalArea(points: Array[Array[Int]]): Int = {
+  val x = points.map(_.head).sorted
+  x.zip(x.tail).flatMap { case (a, b) => Array(b - a) }.max
+}
 
 
 
-val a = Array[(1,2,3,4),(1,5,5,5)]
+val a = Array ((1,25),(1,2))
+//val s = Array(a.map(_.head))
+val v = List(1,2,3,4)
 
-maximumWealth(a)
+v.find(_ == 0)
+
 
 
 
