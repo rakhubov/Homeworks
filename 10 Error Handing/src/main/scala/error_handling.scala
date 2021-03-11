9578a3fea92c0323a245b941bb0b64bd4732bcc0
rakhubov <rakhubov@mgaon.by>
2021-03-11 12:50:28 +0300

    first comit

--- 10 Error Handing/src/main/scala/error_handling.scala
+++ 10 Error Handing/src/main/scala/error_handling.scala
@@ -2,13 +2,13 @@ import scala.math.Ordered.orderingToOrdered
 
 object error_handling {
   case class PaymentCard(
-                          name: String,
-                          number: String,
-                          expirationDate: String,
-                          securityCode: String,
-                          bankName: String,
-                          paymantSystem: String
-                        ) {
+      name: String,
+      number: String,
+      expirationDate: String,
+      securityCode: String,
+      bankName: String,
+      paymantSystem: String
+  ) {
     val youName = name
     val nomberCard = number
     val ExpirationDateCard = expirationDate
@@ -57,7 +57,6 @@ object error_handling {
   import cats.syntax.all._
   import ValidationError._
 
-
   object PaymentCardValidator {
 
     type AllErrorsOr[A] = ValidatedNec[ValidationError, A]
@@ -67,46 +66,47 @@ object error_handling {
       else NameCardLength.invalidNec
     }
 
-
-    def validateNomberCard(nomber: String): AllErrorsOr[String]= {
-      def validateNomberUseFigure: AllErrorsOr[String]=
+    def validateNomberCard(nomber: String): AllErrorsOr[String] = {
+      def validateNomberUseFigure: AllErrorsOr[String] =
         nomber.toIntOption match {
           case Some(value) => nomber.validNec
-          case _ => NomberUseFigure.invalidNec
+          case _           => NomberUseFigure.invalidNec
         }
-      def validateNomberCardLength:AllErrorsOr[String] = {
-        if(BigDecimal(nomber).scale == 16) nomber.validNec
+      def validateNomberCardLength: AllErrorsOr[String] = {
+        if (BigDecimal(nomber).scale == 16) nomber.validNec
         else NomberCardLength.invalidNec
       }
       validateNomberUseFigure *> validateNomberCardLength
     }
 
-
-    def valideteDateCard(date: String): AllErrorsOr[String]={
-      def validateDateTwoNombers:AllErrorsOr[String]= {
-        if(date.split("\\s+").toList.length == 2) date.validNec
+    def valideteDateCard(date: String): AllErrorsOr[String] = {
+      def validateDateTwoNombers: AllErrorsOr[String] = {
+        if (date.split("\\s+").toList.length == 2) date.validNec
         else DateTwoNombers.invalidNec
       }
-      def valideteDateUseNombers:AllErrorsOr[String] =
+      def valideteDateUseNombers: AllErrorsOr[String] =
         date.toIntOption match {
           case Some(value) => date.validNec
-          case _ => DateUseNombers.invalidNec
+          case _           => DateUseNombers.invalidNec
         }
-      def validateDateYearsMounts:AllErrorsOr[String]=
+      def validateDateYearsMounts: AllErrorsOr[String] =
         date.split("\\s+").toList match {
-          case a :: b :: Nil if (a.toIntOption >= Option(1) && a.toIntOption <= Option(12)
-            && b.toIntOption >= Option(16) && b.toIntOption <= Option(21)) => date.validNec
+          case a :: b :: Nil
+              if (a.toIntOption >= Option(1) && a.toIntOption <= Option(12)
+                && b.toIntOption >= Option(16) && b.toIntOption <= Option(
+                21
+              )) =>
+            date.validNec
           case _ => DateYearsMountsValide.invalidNec
         }
       validateDateTwoNombers *> valideteDateUseNombers *> validateDateYearsMounts
     }
 
-
-    def validateSecurityCode(code: String): AllErrorsOr[String] ={
+    def validateSecurityCode(code: String): AllErrorsOr[String] = {
       def validateSecurityCodeUseNombers: AllErrorsOr[String] =
         code.toIntOption match {
           case Some(value) => code.validNec
-          case _ => DateUseNombers.invalidNec
+          case _           => DateUseNombers.invalidNec
         }
       def validateSecurityCode4Nombers: AllErrorsOr[String] =
         if (BigDecimal(code).scale == 4 && code.length == 4) code.validNec
@@ -114,25 +114,32 @@ object error_handling {
       validateSecurityCodeUseNombers *> validateSecurityCode4Nombers
     }
 
-
     def validateBankName(bankName: String): AllErrorsOr[String] =
-      if(bankName == "BelarusBank" || bankName == "BPSBank" || bankName == "MTBBank") bankName.validNec
+      if (
+        bankName == "BelarusBank" || bankName == "BPSBank" || bankName == "MTBBank"
+      ) bankName.validNec
       else BankNameValide.invalidNec
 
-
     def validatePaymentSystem(paymentSystem: String): AllErrorsOr[String] =
-      if(paymentSystem =="Viza" || paymentSystem == "MasterCard") paymentSystem.validNec
+      if (paymentSystem == "Viza" || paymentSystem == "MasterCard")
+        paymentSystem.validNec
       else PaymentSystemValide.invalidNec
 
-
-
-
     def validate(
-                  name: String, number: String, expirationDate: String, securityCode: String,
-                  bankName: String, paymantSystem: String): AllErrorsOr[PaymentCard] =
-      (validateNameCardLength(name), validateNomberCard(number), valideteDateCard(expirationDate),
-        validateSecurityCode(securityCode), validateBankName(bankName), validatePaymentSystem(paymantSystem)).mapN(PaymentCard)
+        name: String,
+        number: String,
+        expirationDate: String,
+        securityCode: String,
+        bankName: String,
+        paymantSystem: String
+    ): AllErrorsOr[PaymentCard] =
+      (
+        validateNameCardLength(name),
+        validateNomberCard(number),
+        valideteDateCard(expirationDate),
+        validateSecurityCode(securityCode),
+        validateBankName(bankName),
+        validatePaymentSystem(paymantSystem)
+      ).mapN(PaymentCard)
   }
 }
-
-
